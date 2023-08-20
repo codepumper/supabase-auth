60e068c5c1f509b188de704251ffd050900bd19f
Robert Kovacs <roberts.kovacs@gmail.com>
2023-08-20 23:27:19 +0200

    Update .prettierrc

--- test/SuppabaseAuth.test.js
+++ test/SuppabaseAuth.test.js
@@ -1,26 +1,26 @@
-import SupabaseAuth from "../src/SupabaseAuth";
-require("dotenv").config();
+import SupabaseAuth from '../lib/SupabaseAuth'
+require('dotenv').config()
 
-describe("SupabaseAuth", () => {
-  const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseKey = process.env.SUPABASE_KEY;
-  const auth = new SupabaseAuth(supabaseUrl, supabaseKey);
+describe('SupabaseAuth', () => {
+  const supabaseUrl = process.env.SUPABASE_URL
+  const supabaseKey = process.env.SUPABASE_KEY
+  const auth = new SupabaseAuth(supabaseUrl, supabaseKey)
 
-  it("should sign up a user", async () => {
-    const { error } = await auth.signUp("user@example.com", "password");
-    expect(error).toBeNull();
-  });
+  it('should sign up a user', async () => {
+    const { error } = await auth.signUp('user@example.com', 'password')
+    expect(error).toBeNull()
+  })
 
-  it("should sign in a user with password", async () => {
+  it('should sign in a user with password', async () => {
     const { error } = await auth.signInWithPassword(
-      "user@example.com",
-      "password"
-    );
-    expect(error).toBeNull();
-  });
+      'user@example.com',
+      'password'
+    )
+    expect(error).toBeNull()
+  })
 
-  it("should sign out a user", async () => {
-    const { error } = await auth.signOut();
-    expect(error).toBeNull();
-  });
-});
+  it('should sign out a user', async () => {
+    const { error } = await auth.signOut()
+    expect(error).toBeNull()
+  })
+})
